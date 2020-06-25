47994d4022bce1a7dc62e3eb69580f9bd9cdb8ca
Patryk Szczypien <patryk.szczypien@gmail.com>
2020-06-25 17:49:34 +0200

    added some roms formatted code with clang-format added opcode callbacks for branching and if's

--- system.c
+++ system.c
@@ -1,164 +1,152 @@
+#include "system.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "system.h"
 #include <time.h>
 
 CHIP8 *init_chip8() {
-	CHIP8 *CHIP8_POINTER = malloc(sizeof *CHIP8_POINTER);
-	if (CHIP8_POINTER == NULL)
-		return NULL;	
-	
-	/* clear whole memory */
-	for (int i = 0; i < MEMORY_SIZE; i++) {
-		MEMORY(i) = 0x00; 
-	}
+  CHIP8 *CHIP8_POINTER = malloc(sizeof *CHIP8_POINTER);
+  if (CHIP8_POINTER == NULL)
+    return NULL;
 
-	/* clear registers */
-	for (int i = 0; i < 16; i++)
-		REGISTER(i) = 0x00;
+  /* clear whole memory */
+  for (int i = 0; i < MEMORY_SIZE; i++) {
+    MEMORY(i) = 0x00;
+  }
 
-	/* fill memory 0x0000 - 0x004F with font data*/
-	for (int i = 0; i < FONT_AMOUNT; i++) {
-		for (int j = 0; j < FONT_SIZE; j++) {		
-			MEMORY(j + (i * FONT_SIZE)) = font[i][j];
-		}
-	}
+  /* clear registers */
+  for (int i = 0; i < 16; i++)
+    REGISTER(i) = 0x00;
 
-	PC = MEMORY_PROGRAM_START;
-	ADDRESS_REGISTER = 0;
-	SP = 0;		
+  /* fill memory 0x0000 - 0x004F with font data*/
+  for (int i = 0; i < FONT_AMOUNT; i++) {
+    for (int j = 0; j < FONT_SIZE; j++) {
+      MEMORY(j + (i * FONT_SIZE)) = font[i][j];
+    }
+  }
 
-	time_t tt;
-	srand(time(&tt));
+  PC = MEMORY_PROGRAM_START;
+  ADDRESS_REGISTER = 0;
+  SP = 0;
 
-	return CHIP8_POINTER;
+  time_t tt;
+  srand(time(&tt));
+
+  return CHIP8_POINTER;
 }
 
 DEFINE_CALL(0NNN) {}
 DEFINE_CALL(00E0) {}
 DEFINE_CALL(00EE) {}
-DEFINE_CALL(1NNN) {}
+DEFINE_CALL(1NNN) { PC = NNN; }
 DEFINE_CALL(2NNN) {}
-DEFINE_CALL(3XNN) {}
-DEFINE_CALL(4XNN) {}
-DEFINE_CALL(5XY0) {}
-
-/** 
-	Vx = NN
-*/
-DEFINE_CALL(6XNN) 
-{
-	REGISTER(VX) = NN;
+DEFINE_CALL(3XNN) {
+  if (REGISTER(VX) == NN)
+    STEP;
 }
-
-/** 
-	Vx = Vx + NN
-*/
-DEFINE_CALL(7XNN) 
-{	
-	REGISTER(VX) += NN;
+DEFINE_CALL(4XNN) {
+  if (REGISTER(VX) != NN)
+    STEP;
 }
-
-/** 
-	Vx = Vy 
-*/
-DEFINE_CALL(8XY0) 
-{
-	REGISTER(VX) = REGISTER(VY);
-}
-
-/** 
-	Vx = Vx OR Vy 
-*/
-DEFINE_CALL(8XY1) 
-{
-	REGISTER(VX) |= REGISTER(VY);
-}
-
-/** 
-	Vx = Vx AND Vy 
-*/
-DEFINE_CALL(8XY2) 
-{
-	REGISTER(VX) &= REGISTER(VY);
-}
-
-/** 
-	Vx = Vx XOR Vy 
-*/
-DEFINE_CALL(8XY3) 
-{
-	REGISTER(VX) ^= REGISTER(VY);
-}
-
-/** 
-	Vx = Vx + Vy
-	set VF carry flag if result > 0xFF 
-*/
-DEFINE_CALL(8XY4) 
-{
-	REGISTER(VX) += REGISTER(VY);
-	REGISTER(VF) = (uint16_t) (REGISTER(VX) + REGISTER(VY)) > 0xFF ? 1 : 0;	
-}
-/** 
-	Vx = Vx - Vy
-	set VF carry flag if Vx < Vy 
-*/
-DEFINE_CALL(8XY5) 
-{
-	REGISTER(VX) -= REGISTER(VY);
-	REGISTER(VF) = REGISTER(VX) < REGISTER(VY) ? 0 : 1;
+DEFINE_CALL(5XY0) {
+  if (REGISTER(VX) == REGISTER(VY))
+    STEP;
 }
 
 /**
-	Vx = Vy >> 1
-	VF = least significant bit prior to shift (0000 0001) 
+        Vx = NN
 */
-DEFINE_CALL(8XY6)
-{
-	REGISTER(VX) = REGISTER(VY) >> 1;	
-	REGISTER(VF) = LSB(VY); 
+DEFINE_CALL(6XNN) { REGISTER(VX) = NN; }
+
+/**
+        Vx = Vx + NN
+*/
+DEFINE_CALL(7XNN) { REGISTER(VX) += NN; }
+
+/**
+        Vx = Vy
+*/
+DEFINE_CALL(8XY0) { REGISTER(VX) = REGISTER(VY); }
+
+/**
+        Vx = Vx OR Vy
+*/
+DEFINE_CALL(8XY1) { REGISTER(VX) |= REGISTER(VY); }
+
+/**
+        Vx = Vx AND Vy
+*/
+DEFINE_CALL(8XY2) { REGISTER(VX) &= REGISTER(VY); }
+
+/**
+        Vx = Vx XOR Vy
+*/
+DEFINE_CALL(8XY3) { REGISTER(VX) ^= REGISTER(VY); }
+
+/**
+        Vx = Vx + Vy
+        set VF carry flag if result > 0xFF
+*/
+DEFINE_CALL(8XY4) {
+  REGISTER(VX) += REGISTER(VY);
+  REGISTER(VF) = (uint16_t)(REGISTER(VX) + REGISTER(VY)) > 0xFF ? 1 : 0;
+}
+/**
+        Vx = Vx - Vy
+        set VF carry flag if Vx < Vy
+*/
+DEFINE_CALL(8XY5) {
+  REGISTER(VX) -= REGISTER(VY);
+  REGISTER(VF) = REGISTER(VX) < REGISTER(VY) ? 0 : 1;
 }
 
-/* 
-	Vx = Vy - Vx
-	set VF carry flag if Vy < Vx 
+/**
+        Vx = Vy >> 1
+        VF = least significant bit prior to shift (0000 0001)
 */
-DEFINE_CALL(8XY7)
-{
-	REGISTER(VX) = REGISTER(VY) - REGISTER(VX);
-	REGISTER(VF) = REGISTER(VY) < REGISTER(VX) ? 0 : 1;	
+DEFINE_CALL(8XY6) {
+  REGISTER(VX) = REGISTER(VY) >> 1;
+  REGISTER(VF) = LSB(VY);
 }
 
-/**  
-	VX = VY << 1
-	VF = most significant bit prior to shift (1000 0000) 
+/*
+        Vx = Vy - Vx
+        set VF carry flag if Vy < Vx
 */
-DEFINE_CALL(8XYE)
-{
-	REGISTER(VX) = REGISTER(VY) << 1;	
-	REGISTER(VF) = MSB(VY);
+DEFINE_CALL(8XY7) {
+  REGISTER(VX) = REGISTER(VY) - REGISTER(VX);
+  REGISTER(VF) = REGISTER(VY) < REGISTER(VX) ? 0 : 1;
 }
 
-DEFINE_CALL(9XY0) {}
+/**
+        VX = VY << 1
+        VF = most significant bit prior to shift (1000 0000)
+*/
+DEFINE_CALL(8XYE) {
+  REGISTER(VX) = REGISTER(VY) << 1;
+  REGISTER(VF) = MSB(VY);
+}
+
+DEFINE_CALL(9XY0) {
+  if (REGISTER(VX) != REGISTER(VY))
+    STEP;
+}
 DEFINE_CALL(ANNN) {}
-DEFINE_CALL(BNNN) {}
+DEFINE_CALL(BNNN) { PC = NNN + REGISTER(V0); }
 
 /**
-	VX = rnd() AND NN
+        VX = rnd() AND NN
 */
-DEFINE_CALL(CXNN)
-{
-	REGISTER(VX) = (uint8_t) (rand() / (RAND_MAX + 1.0) * 0xFF) & NN;
+DEFINE_CALL(CXNN) {
+  REGISTER(VX) = (uint8_t)(rand() / (RAND_MAX + 1.0) * 0xFF) & NN;
 }
 
 DEFINE_CALL(DXYN) {}
 DEFINE_CALL(EX9E) {}
 DEFINE_CALL(EXA1) {}
-DEFINE_CALL(FX07) {}
+DEFINE_CALL(FX07) { REGISTER(VX) = DELAY_TIMER; }
 DEFINE_CALL(FX0A) {}
-DEFINE_CALL(FX15) {}
+DEFINE_CALL(FX15) { DELAY_TIMER = REGISTER(VX); }
 DEFINE_CALL(FX18) {}
 DEFINE_CALL(FX1E) {}
 DEFINE_CALL(FX29) {}
@@ -166,150 +154,184 @@ DEFINE_CALL(FX33) {}
 DEFINE_CALL(FX55) {}
 DEFINE_CALL(FX65) {}
 
-int free_memory(CHIP8 *CHIP8_POINTER) {	
-	free(CHIP8_POINTER);
+int free_memory(CHIP8 *CHIP8_POINTER) {
+  free(CHIP8_POINTER);
 
-	return 0;
+  return 0;
 }
 
-int dump_memory(CHIP8* CHIP8_POINTER) {
-	uint16_t address = 0x00;
-	for (uint16_t i = 0; i < MEMORY_SIZE; i++) {
+int dump_memory(CHIP8 *CHIP8_POINTER) {
+  uint16_t address = 0x00;
+  for (uint16_t i = 0; i < MEMORY_SIZE; i++) {
 
-		if (i % 0x0F == 0) {
-			printf("\n%04X: ", address);
-			address += 0x0F;
-		}
-		printf("%02X ", MEMORY(i));
-	}
+    if (i % 0x0F == 0) {
+      printf("\n%04X: ", address);
+      address += 0x0F;
+    }
+    printf("%02X ", MEMORY(i));
+  }
 
-	return 0;
+  return 0;
 }
 
-int dump_registers(CHIP8* CHIP8_POINTER) {
-	printf("Registers\n");
-	for (int i = 0; i < 16; i++) {
-		printf("V%01X=[%02X]\n", i, REGISTER(i));
-	}
-	printf("PC: %04X", PC);
+int dump_registers(CHIP8 *CHIP8_POINTER) {
+  printf("Registers\n");
+  for (int i = 0; i < 16; i++) {
+    printf("V%01X=[%02X]\n", i, REGISTER(i));
+  }
+  printf("PC: %04X", PC);
 
-	return 0;
+  return 0;
 }
 
-int debug_registers(CHIP8* CHIP8_POINTER) {
+int debug_registers(CHIP8 *CHIP8_POINTER) {
+  printf("[%01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X "
+         "%01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X "
+         "%01X:%02X %01X:%02X %01X:%02X %01X:%02X]",
+         0, REGISTER(V0), 1, REGISTER(V1), 2, REGISTER(V2), 3, REGISTER(V3), 4,
+         REGISTER(V4), 5, REGISTER(V5), 6, REGISTER(V6), 7, REGISTER(V7), 8,
+         REGISTER(V8), 9, REGISTER(V9), 10, REGISTER(VA), 11, REGISTER(VB), 12,
+         REGISTER(VC), 13, REGISTER(VD), 14, REGISTER(VE), 15, REGISTER(VF));
 
-	printf("[%01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X %01X:%02X]",		
-		0, REGISTER(V0),
-		1, REGISTER(V1),
-		2, REGISTER(V2),
-		3, REGISTER(V3),
-		4, REGISTER(V4),
-		5, REGISTER(V5),
-		6, REGISTER(V6),
-		7, REGISTER(V7),
-		8, REGISTER(V8),
-		9, REGISTER(V9),
-		10, REGISTER(VA),
-		11, REGISTER(VB),
-		12, REGISTER(VC),
-		13, REGISTER(VD),
-		14, REGISTER(VE),
-		15, REGISTER(VF)
-	);
-	
-	return 0;
+  return 0;
 }
 
-uint16_t fetch_opcode(CHIP8* CHIP8_POINTER) {	
-	uint16_t opcode = MEMORY(PC++) << 8;
-	opcode |= MEMORY(PC++);
-	
-	return opcode;
+uint16_t fetch_opcode(CHIP8 *CHIP8_POINTER) {
+  uint16_t opcode = MEMORY(PC) << 8;
+  opcode |= MEMORY(PC + 1);
+
+  return opcode;
 }
+
+void step(CHIP8 *CHIP8_POINTER) { PC = PC + 2; }
 
 int run(CHIP8 *CHIP8_POINTER) {
-	
-	DECLARE_OPCODE_VAR;
-	char buffer[50];	
-	
-	for (PC = MEMORY_PROGRAM_START; PC < MEMORY_SIZE;) {
-		OPCODE_VAR = fetch_opcode(CHIP8_POINTER);
 
-		if (IS_OPCODE_GROUP(0)) {
-			if (NN == 0x00E0) { DECODE_00E0(buffer); }
-			else if (NN == 0x00EE) { DECODE_00EE(buffer); }
-			else { DECODE_0NNN(buffer); }
-		}
-		else if (IS_OPCODE_GROUP(1)) { DECODE_1NNN(buffer); }
-		else if (IS_OPCODE_GROUP(2)) { DECODE_2NNN(buffer); }
-		else if (IS_OPCODE_GROUP(3)) { DECODE_3XNN(buffer); }
-		else if (IS_OPCODE_GROUP(4)) { DECODE_4XNN(buffer); }
-		else if (IS_OPCODE_GROUP(5)) { DECODE_5XY0(buffer); }
-		else if (IS_OPCODE_GROUP(6)) { CALL_AND_DECODE(6XNN, buffer); }
-		else if (IS_OPCODE_GROUP(7)) { CALL_AND_DECODE(7XNN, buffer); }
-		else if (IS_OPCODE_GROUP(8)) {
-			if (N == 0x0000) { CALL_AND_DECODE(8XY0, buffer); }
-			else if (N == 0x0001) { CALL_AND_DECODE(8XY1, buffer); }
-			else if (N == 0x0002) { CALL_AND_DECODE(8XY2, buffer); }
-			else if (N == 0x0003) { CALL_AND_DECODE(8XY3, buffer); }
-			else if (N == 0x0004) { CALL_AND_DECODE(8XY4, buffer); }
-			else if (N == 0x0005) { CALL_AND_DECODE(8XY5, buffer); }
-			else if (N == 0x0006) { DECODE_8XY6(buffer); }
-			else if (N == 0x0007) { CALL_AND_DECODE(8XY7, buffer); }
-			else if (N == 0x000E) { DECODE_8XYE(buffer); }
-		}
-		else if (IS_OPCODE_GROUP(9)) { DECODE_9XY0(buffer); }
-		else if (IS_OPCODE_GROUP(A)) { DECODE_ANNN(buffer); }
-		else if (IS_OPCODE_GROUP(B)) { DECODE_BNNN(buffer); }
-		else if (IS_OPCODE_GROUP(C)) { CALL_AND_DECODE(CXNN, buffer); }
-		else if (IS_OPCODE_GROUP(D)) { DECODE_DXYN(buffer); }
-		else if (IS_OPCODE_GROUP(E)) {
-			if (NN == 0x009E) { DECODE_EX9E(buffer); }
-			else if (NN == 0x00A1) { DECODE_EXA1(buffer); }
-		}
-		else if (IS_OPCODE_GROUP(F)) {
-			if (NN == 0x0007) { DECODE_FX07(buffer); }
-			else if (NN == 0x000A) { DECODE_FX0A(buffer); }
-			else if (NN == 0x0015) { DECODE_FX15(buffer); }
-			else if (NN == 0x0018) { DECODE_FX18(buffer); }
-			else if (NN == 0x001E) { DECODE_FX1E(buffer); }
-			else if (NN == 0x0029) { DECODE_FX29(buffer); }
-			else if (NN == 0x0033) { DECODE_FX33(buffer); }
-			else if (NN == 0x0055) { DECODE_FX55(buffer); }
-			else if (NN == 0x0065) { DECODE_FX65(buffer); }
-		}		
-		else {
-			sprintf(buffer, "");
-		}
+  DECLARE_OPCODE_VAR;
+  char buffer[50];
+  DELAY_TIMER = 0x3C;
 
-		if (OPCODE_VAR != 0x0000) {			
-			printf("[$%04X]: %04X --> %s ", PC, OPCODE_VAR, buffer);
-			debug_registers(CHIP8_POINTER);
-			printf("\n");
-		}
-	}
-	return 0;
+  for (PC = MEMORY_PROGRAM_START; PC < MEMORY_SIZE;) {
+    OPCODE_VAR = fetch_opcode(CHIP8_POINTER);
+
+    if (IS_OPCODE_GROUP(0)) {
+      if (NN == 0x00E0) {
+        DECODE_00E0(buffer);
+      } else if (NN == 0x00EE) {
+        DECODE_00EE(buffer);
+      } else {
+        DECODE_0NNN(buffer);
+      }
+    } else if (IS_OPCODE_GROUP(1)) {
+      CALL_AND_DECODE(1NNN, buffer);
+    } else if (IS_OPCODE_GROUP(2)) {
+      DECODE_2NNN(buffer);
+    } else if (IS_OPCODE_GROUP(3)) {
+      CALL_AND_DECODE(3XNN, buffer);
+    } else if (IS_OPCODE_GROUP(4)) {
+      CALL_AND_DECODE(4XNN, buffer);
+    } else if (IS_OPCODE_GROUP(5)) {
+      CALL_AND_DECODE(5XY0, buffer);
+    } else if (IS_OPCODE_GROUP(6)) {
+      CALL_AND_DECODE(6XNN, buffer);
+    } else if (IS_OPCODE_GROUP(7)) {
+      CALL_AND_DECODE(7XNN, buffer);
+    } else if (IS_OPCODE_GROUP(8)) {
+      if (N == 0x0000) {
+        CALL_AND_DECODE(8XY0, buffer);
+      } else if (N == 0x0001) {
+        CALL_AND_DECODE(8XY1, buffer);
+      } else if (N == 0x0002) {
+        CALL_AND_DECODE(8XY2, buffer);
+      } else if (N == 0x0003) {
+        CALL_AND_DECODE(8XY3, buffer);
+      } else if (N == 0x0004) {
+        CALL_AND_DECODE(8XY4, buffer);
+      } else if (N == 0x0005) {
+        CALL_AND_DECODE(8XY5, buffer);
+      } else if (N == 0x0006) {
+        DECODE_8XY6(buffer);
+      } else if (N == 0x0007) {
+        CALL_AND_DECODE(8XY7, buffer);
+      } else if (N == 0x000E) {
+        DECODE_8XYE(buffer);
+      }
+    } else if (IS_OPCODE_GROUP(9)) {
+      CALL_AND_DECODE(9XY0, buffer);
+    } else if (IS_OPCODE_GROUP(A)) {
+      DECODE_ANNN(buffer);
+    } else if (IS_OPCODE_GROUP(B)) {
+      CALL_AND_DECODE(BNNN, buffer);
+    } else if (IS_OPCODE_GROUP(C)) {
+      CALL_AND_DECODE(CXNN, buffer);
+    } else if (IS_OPCODE_GROUP(D)) {
+      DECODE_DXYN(buffer);
+    } else if (IS_OPCODE_GROUP(E)) {
+      if (NN == 0x009E) {
+        DECODE_EX9E(buffer);
+      } else if (NN == 0x00A1) {
+        DECODE_EXA1(buffer);
+      }
+    } else if (IS_OPCODE_GROUP(F)) {
+      if (NN == 0x0007) {
+        CALL_AND_DECODE(FX07, buffer);
+      } else if (NN == 0x000A) {
+        DECODE_FX0A(buffer);
+      } else if (NN == 0x0015) {
+        CALL_AND_DECODE(FX15, buffer);
+      } else if (NN == 0x0018) {
+        DECODE_FX18(buffer);
+      } else if (NN == 0x001E) {
+        DECODE_FX1E(buffer);
+      } else if (NN == 0x0029) {
+        DECODE_FX29(buffer);
+      } else if (NN == 0x0033) {
+        DECODE_FX33(buffer);
+      } else if (NN == 0x0055) {
+        DECODE_FX55(buffer);
+      } else if (NN == 0x0065) {
+        DECODE_FX65(buffer);
+      }
+    } else {
+      sprintf(buffer, "");
+    }
+
+    if (OPCODE_VAR != 0x0000) {
+      printf("[$%04X]: %04X --> %s ", PC, OPCODE_VAR, buffer);
+      debug_registers(CHIP8_POINTER);
+      printf("\n");
+    }
+
+    STEP;
+
+    if (DELAY_TIMER > 0) {
+      DELAY_TIMER--;
+    } else {
+      DELAY_TIMER = 0;
+    }
+  }
+  return 0;
 }
 
-int main(int argc, char** argv) {
-	CHIP8 *CHIP8_POINTER = init_chip8();		
+int main(int argc, char **argv) {
+  CHIP8 *CHIP8_POINTER = init_chip8();
 
-	FILE* rom_file_p = fopen("roms/PONG", "rb");
-	if (!rom_file_p) {
-		printf("Can't open file.\n");
-	}
-	else {
-		uint8_t buffer;
-		uint16_t memory_address = MEMORY_PROGRAM_START;
-		while (fread(&buffer, sizeof buffer, 1, rom_file_p)) {
-			MEMORY(memory_address++) = buffer;
-		};
-		fclose(rom_file_p);
-	}
-	
-	if (dump_memory(CHIP8_POINTER)) printf("Error dumping memory.\n");
-	run(CHIP8_POINTER);
-	dump_registers(CHIP8_POINTER);
-	free_memory(CHIP8_POINTER);
-	return 0;
+  FILE *rom_file_p = fopen("roms/PONG", "rb");
+  if (!rom_file_p) {
+    printf("Can't open file.\n");
+  } else {
+    uint8_t buffer;
+    uint16_t memory_address = MEMORY_PROGRAM_START;
+    while (fread(&buffer, sizeof buffer, 1, rom_file_p)) {
+      MEMORY(memory_address++) = buffer;
+    };
+    fclose(rom_file_p);
+  }
+
+  if (dump_memory(CHIP8_POINTER))
+    printf("Error dumping memory.\n");
+  run(CHIP8_POINTER);
+  dump_registers(CHIP8_POINTER);
+  free_memory(CHIP8_POINTER);
+  return 0;
 }
--- system.h
+++ system.h
@@ -3,30 +3,19 @@
 
 #include <stdint.h>
 
-#define CHIP8_INSTR_SET \
-OP(0NNN) OP(00E0) OP(00EE) \
-OP(1NNN) \
-OP(2NNN) \
-OP(3XNN) \
-OP(4XNN) \
-OP(5XY0) \
-OP(6XNN) \
-OP(7XNN) \
-OP(8XY0) OP(8XY1) OP(8XY2) OP(8XY3) OP(8XY4) OP(8XY5) OP(8XY6) OP(8XY7) OP(8XYE) \
-OP(9XY0) \
-OP(ANNN) \
-OP(BNNN) \
-OP(CXNN) \
-OP(DXYN) \
-OP(EX9E) \
-OP(EXA1) \
-OP(FX07) OP(FX0A) OP(FX15) OP(FX18) OP(FX1E) OP(FX29) OP(FX33) OP(FX55) OP(FX65) 
+#define CHIP8_INSTR_SET                                                        \
+  OP(0NNN)                                                                     \
+  OP(00E0) OP(00EE) OP(1NNN) OP(2NNN) OP(3XNN) OP(4XNN) OP(5XY0) OP(6XNN)      \
+      OP(7XNN) OP(8XY0) OP(8XY1) OP(8XY2) OP(8XY3) OP(8XY4) OP(8XY5) OP(8XY6)  \
+          OP(8XY7) OP(8XYE) OP(9XY0) OP(ANNN) OP(BNNN) OP(CXNN) OP(DXYN)       \
+              OP(EX9E) OP(EXA1) OP(FX07) OP(FX0A) OP(FX15) OP(FX18) OP(FX1E)   \
+                  OP(FX29) OP(FX33) OP(FX55) OP(FX65)
 
-#define MEMORY_SIZE 0x1000             // 4096 bytes memory total
-#define MEMORY_INTERPRETER 0x000       // 000 - 200 : 512 bytes
-#define MEMORY_PROGRAM_START 0x200     // 200 - EA0 : 3232 bytes
-#define MEMORY_CALL_STACK 0xEA0        // EA0 - EFF - 96 bytes 
-#define MEMORY_DISPLAY_REFRESH  0xF00  // F00 - FFF - 256 bytes
+#define MEMORY_SIZE 0x1000           // 4096 bytes memory total
+#define MEMORY_INTERPRETER 0x000     // 000 - 200 : 512 bytes
+#define MEMORY_PROGRAM_START 0x200   // 200 - EA0 : 3232 bytes
+#define MEMORY_CALL_STACK 0xEA0      // EA0 - EFF - 96 bytes
+#define MEMORY_DISPLAY_REFRESH 0xF00 // F00 - FFF - 256 bytes
 
 #define STACK_SIZE 16
 #define REGISTERS_AMOUNT 16
@@ -50,7 +39,7 @@ OP(FX07) OP(FX0A) OP(FX15) OP(FX18) OP(FX1E) OP(FX29) OP(FX33) OP(FX55) OP(FX65)
 
 #define GFX_WIDTH 64
 #define GFX_HEIGHT 32
-#define GFX_RESOLUTION GFX_WIDTH * GFX_HEIGHT
+#define GFX_RESOLUTION GFX_WIDTH *GFX_HEIGHT
 
 #define FONT_AMOUNT 16
 #define FONT_SIZE 5
@@ -58,23 +47,26 @@ OP(FX07) OP(FX0A) OP(FX15) OP(FX18) OP(FX1E) OP(FX29) OP(FX33) OP(FX55) OP(FX65)
 #define CHIP8_POINTER chip8_p
 #define OPCODE_VAR opcode_uint16
 #define DECLARE_OPCODE_VAR uint16_t OPCODE_VAR;
-#define IS_OPCODE_GROUP(n) OPCODE_GROUP(OPCODE_VAR) == OPCODE_ ##n ##xxx
-#define DECLARE_OPCODE_FUNC(opclass) void f_ ##opclass (CHIP8*, uint16_t);
-#define DEFINE_CALL(opclass) void f_ ##opclass (CHIP8* CHIP8_POINTER, uint16_t OPCODE_VAR)
-#define CALL(opcode) Opcode_Callbacks[_ ##opcode ](chip8_p, OPCODE_VAR)
+#define IS_OPCODE_GROUP(n) OPCODE_GROUP(OPCODE_VAR) == OPCODE_##n##xxx
+#define DECLARE_OPCODE_FUNC(opclass) void f_##opclass(CHIP8 *, uint16_t);
+#define DEFINE_CALL(opclass)                                                   \
+  void f_##opclass(CHIP8 *CHIP8_POINTER, uint16_t OPCODE_VAR)
+#define CALL(opcode) Opcode_Callbacks[_##opcode](chip8_p, OPCODE_VAR)
 #define OPCODE_GROUP(opcode) (opcode & 0xF000)
 #define VX (OPCODE_VAR & 0x0FFF) >> 8
 #define VY (OPCODE_VAR & 0x00FF) >> 4
-#define REGISTER(v) CHIP8_POINTER->registers[v] 
+#define DELAY_TIMER CHIP8_POINTER->delay_timer
+#define REGISTER(v) CHIP8_POINTER->registers[v]
 #define MEMORY(address) CHIP8_POINTER->memory[address]
 #define PC CHIP8_POINTER->pc
 #define SP CHIP8_POINTER->sp
 #define ADDRESS_REGISTER CHIP8_POINTER->I
 #define NNN (OPCODE_VAR & 0x0FFF)
-#define NN  (OPCODE_VAR & 0x00FF)
-#define N   (OPCODE_VAR & 0x000F)
+#define NN (OPCODE_VAR & 0x00FF)
+#define N (OPCODE_VAR & 0x000F)
 #define MSB(v) REGISTER(v) && 0xC0
 #define LSB(v) REGISTER(v) && 0x01
+#define STEP step(CHIP8_POINTER);
 
 #define DECODE_0NNN(buffer) sprintf(buffer, "call NNN");
 #define DECODE_00E0(buffer) sprintf(buffer, "clear_display()")
@@ -87,9 +79,12 @@ OP(FX07) OP(FX0A) OP(FX15) OP(FX18) OP(FX1E) OP(FX29) OP(FX33) OP(FX55) OP(FX65)
 #define DECODE_6XNN(buffer) sprintf(buffer, "V%01X = %02X", VX, NN)
 #define DECODE_7XNN(buffer) sprintf(buffer, "V%01X += %02X", VX, NN)
 #define DECODE_8XY0(buffer) sprintf(buffer, "V%01X = V%01X", VX, VY)
-#define DECODE_8XY1(buffer) sprintf(buffer, "V%01X = V%01X OR V%01X", VX, VX, VY)
-#define DECODE_8XY2(buffer) sprintf(buffer, "V%01X = V%01X AND V%01X", VX, VX, VY)
-#define DECODE_8XY3(buffer) sprintf(buffer, "V%01X = V%01X XOR V%01X", VX, VX, VY)
+#define DECODE_8XY1(buffer)                                                    \
+  sprintf(buffer, "V%01X = V%01X OR V%01X", VX, VX, VY)
+#define DECODE_8XY2(buffer)                                                    \
+  sprintf(buffer, "V%01X = V%01X AND V%01X", VX, VX, VY)
+#define DECODE_8XY3(buffer)                                                    \
+  sprintf(buffer, "V%01X = V%01X XOR V%01X", VX, VX, VY)
 #define DECODE_8XY4(buffer) sprintf(buffer, "V%01X = V%01X + V%01X", VX, VX, VY)
 #define DECODE_8XY5(buffer) sprintf(buffer, "V%01X = V%01X - V%01X", VX, VX, VY)
 #define DECODE_8XY6(buffer) sprintf(buffer, "V%01X = V%01X >> 1", VX, VY)
@@ -99,7 +94,8 @@ OP(FX07) OP(FX0A) OP(FX15) OP(FX18) OP(FX1E) OP(FX29) OP(FX33) OP(FX55) OP(FX65)
 #define DECODE_ANNN(buffer) sprintf(buffer, "I = $%04X", NNN)
 #define DECODE_BNNN(buffer) sprintf(buffer, "PC = V0 + $%04X", NNN)
 #define DECODE_CXNN(buffer) sprintf(buffer, "V%01X = rnd() AND %02X", VX, NN)
-#define DECODE_DXYN(buffer) sprintf(buffer, "draw(V%01X, V%01X, %01X)", VX, VY, N)
+#define DECODE_DXYN(buffer)                                                    \
+  sprintf(buffer, "draw(V%01X, V%01X, %01X)", VX, VY, N)
 #define DECODE_EX9E(buffer) sprintf(buffer, "if (key() == V%01X)", VX)
 #define DECODE_EXA1(buffer) sprintf(buffer, "if (key() != V%01X)", VX)
 #define DECODE_FX07(buffer) sprintf(buffer, "V%01X = get_delay_timer()", VX)
@@ -112,9 +108,9 @@ OP(FX07) OP(FX0A) OP(FX15) OP(FX18) OP(FX1E) OP(FX29) OP(FX33) OP(FX55) OP(FX65)
 #define DECODE_FX55(buffer) sprintf(buffer, "dump_registers(V%01X, $I)", VX)
 #define DECODE_FX65(buffer) sprintf(buffer, "load_registers(V%01X, $I)", VX)
 
-#define CALL_AND_DECODE(opcode, buffer)\
-CALL(opcode);\
-DECODE_ ## opcode (buffer);
+#define CALL_AND_DECODE(opcode, buffer)                                        \
+  CALL(opcode);                                                                \
+  DECODE_##opcode(buffer);
 
 #define OPCODE_0xxx 0x0000
 #define OPCODE_1xxx 0x1000
@@ -134,40 +130,41 @@ DECODE_ ## opcode (buffer);
 #define OPCODE_Fxxx 0xF000
 
 uint8_t font[FONT_AMOUNT][FONT_SIZE] = {
-	{0xF0, 0x90, 0x90, 0x90, 0xF0}, /* 0 */
-	{0x20, 0x60, 0x20, 0x20, 0x70}, /* 1 */
-	{0xF0, 0x10, 0xF0, 0x80, 0xF0}, /* 2 */
-	{0xF0, 0x10, 0xF0, 0x10, 0xF0}, /* 3 */
-	{0x90, 0x90, 0xF0, 0x10, 0x10}, /* 4 */
-	{0xF0, 0x80, 0xF0, 0x10, 0xF0}, /* 5 */
-	{0xF0, 0x80, 0xF0, 0x90, 0xF0}, /* 6 */
-	{0xF0, 0x10, 0x20, 0x40, 0x40}, /* 7 */
-	{0xF0, 0x90, 0xF0, 0x90, 0xF0}, /* 8 */
-	{0xF0, 0x90, 0xF0, 0x10, 0xF0}, /* 9 */
-	{0xF0, 0x90, 0xF0, 0x90, 0x90}, /* A */
-	{0xE0, 0x90, 0xE0, 0x90, 0xE0}, /* B */
-	{0xF0, 0x80, 0x80, 0x80, 0xF0}, /* C */
-	{0xE0, 0x90, 0x90, 0x90, 0xE0}, /* D */
-	{0xF0, 0x80, 0xF0, 0x80, 0xF0}, /* E */
-	{0xF0, 0x80, 0xF0, 0x80, 0x80}	/* F */
+    {0xF0, 0x90, 0x90, 0x90, 0xF0}, /* 0 */
+    {0x20, 0x60, 0x20, 0x20, 0x70}, /* 1 */
+    {0xF0, 0x10, 0xF0, 0x80, 0xF0}, /* 2 */
+    {0xF0, 0x10, 0xF0, 0x10, 0xF0}, /* 3 */
+    {0x90, 0x90, 0xF0, 0x10, 0x10}, /* 4 */
+    {0xF0, 0x80, 0xF0, 0x10, 0xF0}, /* 5 */
+    {0xF0, 0x80, 0xF0, 0x90, 0xF0}, /* 6 */
+    {0xF0, 0x10, 0x20, 0x40, 0x40}, /* 7 */
+    {0xF0, 0x90, 0xF0, 0x90, 0xF0}, /* 8 */
+    {0xF0, 0x90, 0xF0, 0x10, 0xF0}, /* 9 */
+    {0xF0, 0x90, 0xF0, 0x90, 0x90}, /* A */
+    {0xE0, 0x90, 0xE0, 0x90, 0xE0}, /* B */
+    {0xF0, 0x80, 0x80, 0x80, 0xF0}, /* C */
+    {0xE0, 0x90, 0x90, 0x90, 0xE0}, /* D */
+    {0xF0, 0x80, 0xF0, 0x80, 0xF0}, /* E */
+    {0xF0, 0x80, 0xF0, 0x80, 0x80}  /* F */
 };
 
 typedef struct {
-	uint8_t memory[MEMORY_SIZE];
-	uint8_t registers[REGISTERS_AMOUNT];
-	uint8_t gfx[GFX_RESOLUTION];
-	uint8_t delay_timer;
-	uint8_t sound_timer;
-	uint8_t input;
-	uint16_t I; // address register
-	uint16_t pc; // program counter
-	uint16_t stack[STACK_SIZE];
-	uint16_t sp; // stack pointer
+  uint8_t memory[MEMORY_SIZE];
+  uint8_t registers[REGISTERS_AMOUNT];
+  uint8_t gfx[GFX_RESOLUTION];
+  uint8_t delay_timer;
+  uint8_t sound_timer;
+  uint8_t input;
+  uint16_t I;  // address register
+  uint16_t pc; // program counter
+  uint16_t stack[STACK_SIZE];
+  uint16_t sp; // stack pointer
 } CHIP8;
 
+void step(CHIP8*);
 
 /* Declare opcode callback function type -> void F(chip8_handler, opcode) */
-typedef void (* const opcode_callback_f) (CHIP8*, const uint16_t);
+typedef void (*const opcode_callback_f)(CHIP8 *, const uint16_t);
 
 /* Declare a C function for each opcode */
 #define OP(x) DECLARE_OPCODE_FUNC(x);
@@ -175,12 +172,12 @@ CHIP8_INSTR_SET;
 #undef OP
 
 /* Declare array of callback functions assigned to CHIP-8 opcodes */
-#define OP(x) f_ ## x,
-static opcode_callback_f Opcode_Callbacks[] = { CHIP8_INSTR_SET };
+#define OP(x) f_##x,
+static opcode_callback_f Opcode_Callbacks[] = {CHIP8_INSTR_SET};
 #undef OP
 
 /* Declare enumeration of CHIP-8 opcodes */
-#define OP(x) _ ## x,
+#define OP(x) _##x,
 enum opcodes_order_e { CHIP8_INSTR_SET };
 #undef OP
 
